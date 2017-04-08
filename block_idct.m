ebba6424b956e5bfef87ffa138bd2e46578084f9
tiffany-yang <tiffany61706@yahoo.com.tw>
2017-04-08 16:16:53 +0800

    test

---
+++ block_idct.m
@@ -0,0 +1,13 @@
+function [DCT_iTrans] = block_idct(DCT_embed, block_size)
+
+[row,col] = size(DCT_embed);
+DCT_iTrans = zeros(row,col);
+for i = 1:block_size:row-block_size+1
+    for j = 1:block_size:col-block_size+1
+        A = DCT_embed(i:i+block_size-1,j:j+block_size-1);
+        B = idct2(A);
+        DCT_iTrans(i:i+block_size-1,j:j+block_size-1) = B;
+    end
+end
+
+end
